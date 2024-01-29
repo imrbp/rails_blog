0e1b75604e42fad2833f1fcd658ba8fbb1e78d8f
I Made Raditya Bayu Pangestu <63552637+imrbp@users.noreply.github.com>
2024-01-29 18:54:10 +0700

    add pagination

--- app/controllers/blog_post_controller.rb
+++ app/controllers/blog_post_controller.rb
@@ -4,6 +4,11 @@ class BlogPostController < ApplicationController
 
   def index
     @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
+
+    @pagy, @blog_posts = pagy(@blog_posts)
+  rescue Pagy::OverflowError
+    params[:page] = 1
+    retry
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+#   
+user = User.where(email: "superadmin@blog.rails").first_or_initialize
+user.update!(
+    password: "12345678",
+    password_confirmation: "12345678"
+)
+
+# 100.times do |i|
+#   BlogPost.create title: "Blog Post #{i}", content: "This is content for this blog post #{i}"
+# end
